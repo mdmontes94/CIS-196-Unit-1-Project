5e681a8789956955c7ee34c246e77b3097057e6d
Mario Montes <mdmontes94@gmail.com>
2024-02-05 11:27:34 -0600

    Added images to the folder and one aspect of intended javascript.

--- project.js
+++ project.js
@@ -1,2 +1,14 @@
 "use strict";
 
+var goofyDiv = document.querySelector("#goofy");
+
+var num = prompt("Please pick a number between 1 and 10");
+var gawrsh = parseInt(num);
+
+for (let i = 1; i <= gawrsh; i++)
+{
+    var newParagraph = document.createElement("h3");
+    newParagraph.textContent = "Gawrsh!";
+
+    goofyDiv.appendChild(newParagraph);
+}
