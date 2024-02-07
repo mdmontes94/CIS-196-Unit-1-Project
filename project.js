f00b5f7c2ea230c3a57c18f853a6e55ed1140310
Mario Montes <mdmontes94@gmail.com>
2024-02-07 10:49:14 -0600

    Removed buttons div and finished project.js script

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var goofyDiv = document.querySelector("#goofy");
+const select = selector => document.querySelector(selector);
 
 var num = prompt("Please pick a number between 1 and 10");
 var gawrsh = parseInt(num);
@@ -10,5 +10,25 @@ for (let i = 1; i <= gawrsh; i++)
     var newParagraph = document.createElement("h3");
     newParagraph.textContent = "Gawrsh!";
 
-    goofyDiv.appendChild(newParagraph);
+    select("#goofy").appendChild(newParagraph);
 }
+
+const grumpierDonald = () =>
+{
+    document.body.style.backgroundColor = "red";
+    select("#grumpy").src = "angry.png";
+
+   document.getElementById("header").textContent = "Oh no! We have made him angrier! Let's help put him in a better mood. (Hint: Double click)";
+};
+
+const happyDonald = () =>
+{
+    document.body.style.backgroundColor = "skyblue";
+    select("#grumpy").src = "happy.png";
+
+    document.getElementById("header").textContent = "Hooray! You've helped make him happy. Great job!";
+};
+
+select("#grumpy").addEventListener("click", grumpierDonald);
+
+select("#grumpy").addEventListener("dblclick", happyDonald);
